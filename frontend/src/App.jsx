c9da4b27210b26bcfd3f3a933b7229ad3b066a36
Suho34 <suhaibnisar01@gmail.com>
2025-08-14 09:34:53 +0530

    remove unused NoteDetailPage route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
 import CreatePage from "./pages/CreatePage.jsx";
-import NoteDetailPage from "./pages/NoteDetailPage.jsx";
 import EditNote from "./pages/EditNote.jsx";
 const App = () => {
   return (
@@ -9,7 +8,6 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
-        <Route path="/notes/:id" element={<NoteDetailPage />} />
         <Route path="/notes/edit/:id" element={<EditNote />} />
       </Routes>
     </div>
